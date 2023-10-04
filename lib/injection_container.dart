d7ae42ebc2c7377c843ca85e253e1ec1545b2bd5
henry-azer <henryazer@outlook.com>
2023-10-04 20:48:02 +0300

    get rid of unused dependencies and utils

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -4,13 +4,8 @@ import 'package:habit_it/core/managers/biometric-authentication/biometric_auth_m
 import 'package:habit_it/core/managers/biometric-authentication/i_biometric_auth_manager.dart';
 import 'package:habit_it/core/managers/storage-manager/i_storage_manager.dart';
 import 'package:habit_it/core/managers/storage-manager/local_storage_manager.dart';
-import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:local_auth/local_auth.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'core/api/api_consumer.dart';
-import 'core/api/app_interceptors.dart';
-import 'core/api/dio_consumer.dart';
-import 'core/network/network_info.dart';
 import 'data/datasources/authentication/authentication_local_datasource.dart';
 import 'data/datasources/user/user_local_datasource.dart';
 
@@ -19,31 +14,13 @@ final sl = GetIt.instance;
 Future<void> init() async {
 
   // !---- Data Sources ----!
-  // authentication
-  sl.registerLazySingleton<AuthenticationLocalDataSource>(() => AuthenticationLocalDataSourceImpl(storageManager: sl()));
-  // user
   sl.registerLazySingleton<UserLocalDataSource>(() => UserLocalDataSourceImpl(storageManager: sl()));
-
-
-  // !---- Core ----!
-  sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(connectionChecker: sl()));
-  sl.registerLazySingleton<ApiConsumer>(() => DioConsumer(client: sl()));
-
+  sl.registerLazySingleton<AuthenticationLocalDataSource>(() => AuthenticationLocalDataSourceImpl(storageManager: sl()));
 
   // !---- External ----!
-  final sharedPreferences = await SharedPreferences.getInstance();
-  sl.registerLazySingleton(() => sharedPreferences);
-  sl.registerLazySingleton(() => AppInterceptors());
-  sl.registerLazySingleton(() => LogInterceptor(
-      request: true,
-      requestBody: true,
-      requestHeader: true,
-      responseBody: true,
-      responseHeader: true,
-      error: true));
+  sl.registerLazySingleton(() => Dio());
+  sl.registerLazySingleton(() => SharedPreferences.getInstance());
   sl.registerLazySingleton<IStorageManager>(() => LocalStorageManager());
   sl.registerLazySingleton<LocalAuthentication>(() => LocalAuthentication());
   sl.registerLazySingleton<IBiometricAuthenticationManager>(() => BiometricAuthenticationManager(localAuthentication: sl(), authenticationLocalDataSource: sl()));
-  sl.registerLazySingleton(() => InternetConnectionChecker());
-  sl.registerLazySingleton(() => Dio());
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:habit_it/app.dart';
-import 'bloc_observer.dart';
 import 'injection_container.dart' as di;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await di.init();
-  Bloc.observer = AppBlocObserver();
   runApp(const HabitItApp());
 }

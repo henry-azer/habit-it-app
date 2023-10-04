f12e142e2048f037c03d396a0172aa6794a69100
henry-azer <henryazer@outlook.com>
2023-10-04 19:59:00 +0300

    add functionalities to Date Util

--- lib/core/utils/date_util.dart
+++ lib/core/utils/date_util.dart
@@ -1,10 +1,34 @@
-// ignore_for_file: depend_on_referenced_packages
-import 'package:intl/intl.dart';
-
 class DateUtil {
-  static String? convertStringDateTimeToStringDate(String stringDateTime) {
-    var dateTime = DateTime.tryParse(stringDateTime);
-    if (dateTime != null) return DateFormat('yyyy-MM-dd').format(dateTime);
-    else return stringDateTime;
+  static DateTime now = DateTime.now();
+
+  static String getCurrentMonthString() {
+    return "${now.year}-${now.month}";
   }
+
+  static String getCurrentDayString() {
+    return "${now.year}-${now.month}-${now.day}";
+  }
+
+  static DateTime getTodayDate() {
+    return DateTime(now.year, now.month, now.day + 5, 0);
+  }
+
+  static DateTime getFirstDayOfCurrentMonth() {
+    return DateTime(now.year, now.month, 1);
+  }
+
+  static List<DateTime> getCurrentMonthDays() {
+    DateTime now = DateTime.now();
+    List<DateTime> monthDaysList = [];
+    DateTime firstDayOfCurrentMonth = DateTime(now.year, now.month, 1, 0);
+    DateTime todayDate = DateTime(now.year, now.month, now.day, 0);
+
+    for (int i = 0; i < todayDate.day + 5; i++) {
+      DateTime currentDay = firstDayOfCurrentMonth.add(Duration(days: i));
+      monthDaysList.add(DateTime(currentDay.year, currentDay.month, currentDay.day, 0));
+    }
+
+    return monthDaysList.reversed.toList();
+  }
+
 }
